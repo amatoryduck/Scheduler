fc4747bca9322f57550cf1e64fe16fb4127e500b
amatoryduck <alexander.n.scott@gmail.com>
2019-08-20 13:30:09 -0400

    Update texer.py

--- texer.py
+++ texer.py
@@ -5,18 +5,24 @@ used_dates = list()
 out = open("outtex.txt", "w")
 hline = r"\hline"
 
+# Read out.txt
 with open("./out.txt") as f:
     content = f.readlines()
 
+# Add lines to array
+# This is an array of subarrays organized by date
+# that containt arrays of lines split by tabs
 for line in content:
 	if line.split("\t")[0] not in used_dates:
 		used_dates.append(line.split("\t")[0])
 		tsv.append(list())    
 	tsv[-1].append(line.split("\t"))
 
+# Get start and end dates
 first = tsv[0][0][0]
 last = tsv[len(tsv) - 1][0][0]
 
+# Write beginning of document
 l = r"\documentclass{article}"
 out.write(l)
 out.write("\n")
@@ -37,29 +43,26 @@ out.write("\n")
 l = r"\maketitle"
 out.write(l)
 out.write("\n")
-
 l = r"\newpage"
 out.write(l)
 out.write("\n")
-
 l = r"\begin{table}[h]"
 out.write(l)
 out.write("\n")
-
 l = r"\begin{tabular}{|c|c|L|L|}"
 out.write(l)
 out.write("\n")
-
+out.write(hline)
+out.write("\n")
 l = r"Time & Date & Show & Series \\"
 out.write(l)
 out.write("\n")
 out.write(hline)
 out.write("\n")
 
+# Write all the data to file
 count = 1
 for i in tsv:
-	out.write(hline)
-	out.write("\n")
 	for j in i:
 		count = count + 1
 		l = r"" + j[1] + " & " + j[0] + " & " + j[3] + " & " + j[4] + r"\\"
@@ -81,17 +84,18 @@ for i in tsv:
 			l = r"\begin{table}[hp]"
 			out.write(l)
 			out.write("\n")
-			l = r"\begin{tabular}{|c|c|L|L}"
-			out.write(l)
-			out.write("\n")
-			l = r"Time & Show & Series \\"
+			l = r"\begin{tabular}{|c|c|L|L|}"
 			out.write(l)
 			out.write("\n")
 			out.write(hline)
+			out.write("\n")
+			l = r"Time & Date & Show & Series \\"
+			out.write(l)
 			out.write("\n")
 			out.write(hline)
 			out.write("\n")
 
+# Write end of document
 l = r"\end{tabular}"
 out.write(l)
 out.write("\n")
